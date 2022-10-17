973012f913531209dffaba795523c18998d1638b
Jordan Leiber <jordan.leiber@vertiq.co>
2022-10-17 14:21:46 -0400

    Update to always find the entry with 115200 baud to use as the default

--- App/mainwindow.cpp
+++ App/mainwindow.cpp
@@ -56,8 +56,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     iv.pcon->FindBaudrates();
     //Because we set motors to an initial baud rate of 115200, we should display that as the default value in order
     //to reduce the number of clicks the user has to make in order to connect with the motor
-    ui->header_baudrate_combo_box->setCurrentIndex(1); //Set first shown value to 115200
-    iv.pcon->BaudrateComboBoxIndexChanged(1); //Actually select the value as 115200 
+    int index115200 = ui->header_baudrate_combo_box->findText("115200"); 
+    ui->header_baudrate_combo_box->setCurrentIndex(index115200); //Set first shown value to 115200
+    iv.pcon->BaudrateComboBoxIndexChanged(index115200); //Actually select the value as 115200 
 
     connect(iv.pcon, SIGNAL(LostConnection()), this, SLOT(ClearTabs()));
     tab_populator = new TabPopulator(ui, &tab_map_);
