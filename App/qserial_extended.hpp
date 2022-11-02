fad5a201bb552643335f732f555ec3a16ffab327
Jordan Leiber <jordan.leiber@vertiq.co>
2022-11-02 15:58:12 -0400

    Made qserial extended a child of both qserial and qserial interface. Still want to do more cleanup

--- App/qserial_extended.hpp
+++ App/qserial_extended.hpp
@@ -3,6 +3,7 @@
 
 #include "qserial_interface.h"
 #include "schmi/include/Schmi/std_exception.hpp"
+#include "schmi/include/Schmi/qserial.h"
 
 namespace SerialConst {
 const qint32 BAUD_RATE = QSerialPort::Baud115200;
@@ -14,11 +15,12 @@ struct SerialReadData {
 };
 
 //This class is going to live below QSerialInterface, and will add core read and write operations to the serial line
-class QSerialExtended : public QSerialInterface{
+class QSerialExtended : public QSerialInterface, public Schmi::QSerial{
 
 public:
     QSerialExtended(const QString& usb_handle, const qint32 &baud_rate = 115200):
         QSerialInterface(usb_handle, baud_rate),  //Let the top class make and open the serial port
+        Schmi::QSerial(ser_port_),
         qser_port_(ser_port_),
         usb_handle_(usb_handle)
     {
@@ -26,18 +28,18 @@ public:
 
     ~QSerialExtended();
 
-    int Write(uint8_t* buffer, const uint16_t& buffer_length);
-    int Read(uint8_t* buffer, const uint16_t& num_bytes, const uint16_t& timeout_ms = 500);
-    void Init();
+//    int Write(uint8_t* buffer, const uint16_t& buffer_length);
+//    int Read(uint8_t* buffer, const uint16_t& num_bytes, const uint16_t& timeout_ms = 500);
+//    void Init();
 
     QSerialPort* qser_port_;
 
 private:
     QString usb_handle_;
 
-    void CheckNumBytesWritten(const qint64& num_bytes_written, const uint16_t& buffer_length);
-    void CheckNumBytesRead(const qint64& num_bytes_read);
-    void UpdateSerialReadData(SerialReadData& read_data, const qint64& num_bytes_read);
+//    void CheckNumBytesWritten(const qint64& num_bytes_written, const uint16_t& buffer_length);
+//    void CheckNumBytesRead(const qint64& num_bytes_read);
+//    void UpdateSerialReadData(SerialReadData& read_data, const qint64& num_bytes_read);
 };
 
 #endif // QSERIALEXTENDED_HPP
