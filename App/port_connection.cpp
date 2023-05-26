8556b788c649d0364db4e391dc647ab38bdf832d
Jordan Leiber <jordan.leiber@vertiq.co>
2023-05-26 15:21:10 -0400

    updated GetEntryReply call to match merged in develop

--- App/port_connection.cpp
+++ App/port_connection.cpp
@@ -403,7 +403,7 @@ bool PortConnection::CheckIfInBootLoader(){
 }
 
 void PortConnection::GetUidValues(uint32_t * uid1, uint32_t * uid2, uint32_t * uid3){
-    GetEntryReply(*ser_, sys_map_["system_control_client"], "uid1", 5, 0.05f, *uid1);
-    GetEntryReply(*ser_, sys_map_["system_control_client"], "uid2", 5, 0.05f, *uid2);
-    GetEntryReply(*ser_, sys_map_["system_control_client"], "uid3", 5, 0.05f, *uid3);
+    GetEntryReply(ser_, sys_map_["system_control_client"], "uid1", 5, 0.05f, *uid1);
+    GetEntryReply(ser_, sys_map_["system_control_client"], "uid2", 5, 0.05f, *uid2);
+    GetEntryReply(ser_, sys_map_["system_control_client"], "uid3", 5, 0.05f, *uid3);
 }
