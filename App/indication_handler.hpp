f4d0353e9d003ca5a4e77a6b9ed01126274039f1
Jordan Leiber <jordan.leiber@vertiq.co>
2023-09-08 09:08:03 -0400

    added more song definitions to allow us to pick one later if we want. also opens up the ability to let people pick which song they want.

--- App/indication_handler.hpp
+++ App/indication_handler.hpp
@@ -5,7 +5,7 @@
 #include "IQ_api/client.hpp"
 #include <math.h>
 
-#define USING_FOLKSONG
+#define USING_BB_TRIAD
 
 class IndicationHandler : public QObject {
   Q_OBJECT
@@ -17,31 +17,37 @@ class IndicationHandler : public QObject {
  private:
   //notes that we can use (rounded...so they're not exactly the right note)
   const uint16_t c = (uint16_t)floor(261.6 * 2);
+  const uint16_t low_c = (uint16_t)floor(261.6);
   const uint16_t d = (uint16_t)floor(293.665 * 2);
   const uint16_t e = (uint16_t)floor(329.7 * 2);
+  const uint16_t low_e = (uint16_t)floor(329.7);
   const uint16_t f = (uint16_t)floor(349.2 * 2);
   const uint16_t g = (uint16_t)floor(392 * 2);
+  const uint16_t low_g = (uint16_t)floor(392);
   const uint16_t a = (uint16_t)floor(440 * 2);
   const uint16_t b = (uint16_t)floor(493 * 2);
+  const uint16_t low_b = (uint16_t)floor(493);
   const uint16_t ab = (uint16_t)floor(207.65);
   const uint16_t db = (uint16_t)floor(277.18);
   const uint16_t eb = (uint16_t)floor(311.127);
   const uint16_t bb = (uint16_t)floor(466.164);
   const uint16_t fs = (uint16_t)floor(369.99);
+  const uint16_t high_fs = (uint16_t)floor(369.99 * 2);
 
 #ifdef USING_BB_TRIAD
-
   const uint16_t indication_speed = 100;
   const uint16_t indication_notes[9] = {bb, d, f, d, bb, d, f, d, bb};
   const uint16_t indication_durations[9] = {indication_speed, indication_speed, indication_speed, indication_speed, indication_speed, indication_speed, indication_speed, indication_speed, indication_speed};
-
 #elif defined(USING_FOLKSONG)
-
   const uint16_t indication_speed = 125;
   const uint16_t indication_notes[5] = {d,e,d,e,g};
   const uint16_t indication_durations[5] = {indication_speed, indication_speed, indication_speed, indication_speed, indication_speed};
-
+#elif defined(USING_FANCY_CHORD)
+  const uint16_t indication_speed = 175;
+  const uint16_t indication_notes[6] = {low_c, low_e, low_g, low_b, d, high_fs};
+  const uint16_t indication_durations[6] = {indication_speed, indication_speed, indication_speed, indication_speed, indication_speed, indication_speed};
 #endif
+
 
   void PlayNote(uint16_t frequency, uint16_t duration);
 
