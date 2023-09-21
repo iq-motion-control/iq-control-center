c4e71b1a74856a5a378bf0eea427bb49bdb676e2
Jordan Leiber <jordan.leiber@vertiq.co>
2023-09-21 08:49:56 -0400

    Fixed a comment

--- App/firmware.cpp
+++ App/firmware.cpp
@@ -530,7 +530,7 @@ bool Firmware::BootMode() {
         //        QCoreApplication::processEvents();
       }
 
-      //We need to kill our connection, so we should also kill our known connections
+      //We need to kill our connection, so we should also kill our known detections
       iv.pcon->ClearDetections();
 
       // delete and unconnects port
