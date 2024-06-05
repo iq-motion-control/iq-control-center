a5b1a4ba6d7af1447a177db203816b1d45083812
Ben <ben.quan@vertiq.co>
2024-06-05 12:47:17 -0400

    message box added displayMessageBox() to let user know resource files were imported successfully and to restart application added setFilePermissions() function to set file permissions using QTemporaryFile

--- App/resource_pack.cpp
+++ App/resource_pack.cpp
@@ -1,21 +1,20 @@
-#include "resource_pack.h"
 #include "main.h"
+#include "resource_pack.h"
 
 ResourcePack::ResourcePack(){
   // Get current application path to use for importing Resource Pack.
   currentAppPath = QCoreApplication::applicationDirPath();
 }
 
-void ResourcePack::importResourcePackFromPath(QString zipFileToImport) {
-  QFileInfo fileInfo(zipFileToImport);
+void ResourcePack::importResourcePackFromPath(QString zipFilePath) {
+  QFileInfo fileInfo(zipFilePath);
   // Get the base name of the zip file, which is the name of the file minus '.zip'.
   resourcePackBaseName = fileInfo.baseName();
 
-  if(zipFileToImport!= ""){
-    iv.pcon->AddToLog("Resource Pack selected: " + zipFileToImport);
-    iv.pcon->AddToLog("Temp resource pack dir: " + resourcePackExtractPath);
-
+  if(zipFilePath != ""){
+    iv.pcon->AddToLog("Resource Pack selected: " + zipFilePath);
     JlCompress extractTool;
+
     // Create a temporary directory to store the extracted files. This object gets automatically deleted once out of scope.
     QTemporaryDir tempDir;
     if(tempDir.isValid()){
@@ -23,7 +22,7 @@ void ResourcePack::importResourcePackFromPath(QString zipFileToImport) {
       iv.pcon->AddToLog("Temporary directory path:" + resourcePackExtractPath);
 
       // Extract contents of .zip file into temporary directory.
-      extractTool.extractDir(zipFileToImport, resourcePackExtractPath);
+      extractTool.extractDir(zipFilePath, resourcePackExtractPath);
       QDirIterator dirIt(resourcePackExtractPath, QDirIterator::Subdirectories);
 
       // Iterate through each resource file in the temporary directory and copy to the main Resources directory
@@ -32,15 +31,16 @@ void ResourcePack::importResourcePackFromPath(QString zipFileToImport) {
         if(filePath.contains(".json")){
             // Get the path of each resource file.
             QStringList splitPath = filePath.split(resourcePackBaseName);
-            //If it's a real file go and change all of the possible read permissions to make sure we can access them.
-            updateResourceFilePermissions(filePath);
-            iv.pcon->AddToLog("Updated permissions for: " + splitPath[1]);
+
+            // Set the appropriate read permissions to make sure they can be accessed.
+            setFilePermissions(filePath);
 
             QString resourcesPath = currentAppPath + "/Resources" + splitPath[1];
             iv.pcon->AddToLog("Copying file: " + filePath + " to: " + resourcesPath);
             QFile::copy(filePath, resourcesPath);
         }
       }
+      displayMessageBox();
     }else{
       iv.pcon->AddToLog("Temporary directory not valid!");
     }
@@ -49,8 +49,25 @@ void ResourcePack::importResourcePackFromPath(QString zipFileToImport) {
   }
 }
 
-void ResourcePack::updateResourceFilePermissions(QString filePath){
-  QFile * tempFile = new QFile(filePath);
-  tempFile->setPermissions(filePath, QFileDevice::ReadOwner | QFileDevice::ReadGroup | QFileDevice::ReadOther | QFileDevice::ReadUser);
-  delete tempFile;
+void ResourcePack::displayMessageBox(){
+      QMessageBox msgBox;
+      msgBox.setWindowTitle("Application restart required");
+
+      QString msg;
+      msg = "Resource files imported successfully. Please close and restart the application to properly load the imported resource files.";
+      msgBox.setText(msg);
+      msgBox.setStandardButtons(QMessageBox::Ok);
+      msgBox.setDefaultButton(QMessageBox::Ok);
+      iv.pcon->AddToLog(msg);
+
+      if(msgBox.exec() == QMessageBox::Ok){
+          return;
+      }
+}
+
+void ResourcePack::setFilePermissions(QString filePath){
+    // Using a QTemporaryFile object because it automatically gets deleted once out of scope.
+    QTemporaryFile tempFile(filePath);
+    tempFile.setPermissions(filePath, QFileDevice::ReadOwner | QFileDevice::ReadGroup | QFileDevice::ReadOther | QFileDevice::ReadUser);
+    iv.pcon->AddToLog("Updated permissions for: " + filePath);
 }
--- App/resource_pack.h
+++ App/resource_pack.h
@@ -1,28 +1,28 @@
 #ifndef RESOURCE_PACK_H
 #define RESOURCE_PACK_H
 
-#include <QObject>
 #include <JlCompress.h>
 
 #include <QDirIterator>
-
 #include <QFile>
 #include <QFileDialog>
-#include <QStandardPaths>
+#include <QObject>
 #include <QTemporaryDir>
+#include <QTemporaryFile>
 
 class ResourcePack : public QObject{
     Q_OBJECT
 
   public:
     ResourcePack();
-    void importResourcePackFromPath(QString zipFileToImport);
+    void importResourcePackFromPath(QString zipFilePath);
 
   private:
     QString currentAppPath;
     QString resourcePackExtractPath;
     QString resourcePackBaseName;
-    void updateResourceFilePermissions(QString filePath);
+    void displayMessageBox();
+    void setFilePermissions(QString filePath);
 
 };
 
