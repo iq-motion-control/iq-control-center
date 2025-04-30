76e4f2d9d0d62924b3a1cdec105ad78c7a0e06f6
Ben <ben.quan@vertiq.co>
2025-04-30 15:54:57 -0400

    Updated importResourcePackFromPath in resource_pack.cpp to extract resource files into ImportedResourceFiles without the zip file's basename and copy file over to SessionResourceFiles

--- App/resource_pack.cpp
+++ App/resource_pack.cpp
@@ -23,31 +23,44 @@ void ResourcePack::importResourcePackFromPath(QString zipFilePath) {
     iv.pcon->AddToLog("Selected .zip file: " + zipFilePath);
 
     QFileInfo zipFileInfo(zipFilePath);
-    // Get the base name of the zip file, which is the name of the file minus '.zip'.
+    // Get the base name of the .zip file, which is the name of the file minus '.zip'.
     QString zipFileBaseName = zipFileInfo.baseName();
 
     if(appDataImportedResourcesDirectory.exists()){
       JlCompress extractTool;
+      // Get the list of paths in the .zip file
       QStringList resourcePackFileList = extractTool.getFileList(zipFilePath);
 
       iv.pcon->AddToLog("Extracting from: " + zipFilePath + " to: " + appDataImportedResourcesPath);
-      // Extracts contents from zip file to ImportedResourceFiles in AppData
-      QStringList extractedResourceFiles = extractTool.extractDir(zipFilePath, appDataImportedResourcesPath);
 
-      // Copy extracted files into SessionResourceFiles
-      if(!extractedResourceFiles.isEmpty()){
+      // Iterate through the resource pack file list and extracted each file into ImportedResourceFiles
+      if(!resourcePackFileList.isEmpty()){
         // Use qAsConst() here to apply const to QStringList because QtCreator complains
-        for(const QString& resourceFile : qAsConst(extractedResourceFiles)) {
-          int appDataImportedResourcesPathLength = appDataImportedResourcesPath.length() + zipFileBaseName.length() + 1; // add 1 to include the trailing slash after the basename
-          // Get the path of the imported resource file only keeping everything after the zipFileBaseName
-          QString fileName = resourceFile.mid(appDataImportedResourcesPathLength);
-          QFileInfo fileInfo(resourceFile);
+        for(const QString& resourceFile : qAsConst(resourcePackFileList)) {
+          int zipFileBaseNameLength = zipFileBaseName.length() + 1; // Add 1 to length to include trailing slash
+          // Get the path of the resource file to be imported only keeping everything after the zipFileBaseName
+          // We don't want to keep the top-level directory as the base name of the .zip file, we only want the subdirectories that contain the resource files
+          QString resourceFileName = resourceFile.mid(zipFileBaseNameLength);
+
+          // Construct the destination path where the extracted resource file will be stored in ImportedResourceFiles
+          QString destinationImportedResourcesPath = appDataImportedResourcesPath + resourceFileName;
+
+          // Extract resource file from .zip file to ImportedResourceFiles in AppData
+          iv.pcon->AddToLog("Extracting Resource file: " + resourceFile + " to: " + destinationImportedResourcesPath);
+
+          // extractFile() will return the path of the extracted resource file at the destination
+          // This will extract the resource file and store it in ImportedResourceFiles without the top-level base name directory of the .zip file
+          QString extractedFilePath = extractTool.extractFile(zipFilePath, resourceFile, destinationImportedResourcesPath);
+
+          // Create a QFileInfo object to check if the extracted resource file is a file before copying over to SessionResourceFiles
+          QFileInfo fileInfo(extractedFilePath);
           if(fileInfo.isFile()){
-            QString destinationFilePath = appDataSessionResourcesPath + fileName;
-            iv.pcon->AddToLog("Copying Resource file: " + resourceFile + " to: " + destinationFilePath);
+            QString destinationSessionResourcesPath = appDataSessionResourcesPath + resourceFileName;
+            iv.pcon->AddToLog("Copying Resource file: " + extractedFilePath + " to: " + destinationSessionResourcesPath);
+
             // Remove existing resource file, new resource file will overwrite any existing ones
-            QFile::remove(destinationFilePath);
-            QFile::copy(resourceFile, destinationFilePath);
+            QFile::remove(destinationSessionResourcesPath);
+            QFile::copy(extractedFilePath, destinationSessionResourcesPath);
           }
         }
         iv.pcon->AddToLog("Extracted Resource Files");
